7a4d7546219cccc5fdbff0164b49ba0fb37053be
TripelA <anamay9707@gmail.com>
2020-04-24 22:05:17 -0400

    Update Google_api.py

--- code/Google_api.py
+++ code/Google_api.py
@@ -1,13 +1,20 @@
-from google.cloud import speech_v1
+# Import packages
 import io
 import os
+import numpy as np
+
 os.system("sudo -H pip install --upgrade python-Levenshtein")
 import Levenshtein
 from tqdm import tqdm
-import numpy as np
+from google.cloud import speech_v1  # python wrapper for google cloud API calls
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getcwd()+'/Machine-Learning-2-bdc5dbdf312d.json' # add own credentials
+# Assign google cloud API credentials variable to personal credentials file
+os.environ[
+    'GOOGLE_APPLICATION_CREDENTIALS'] = os.getcwd() + '/Machine-Learning-2-bdc5dbdf312d.json'  # add own credentials
+
+# dummy API call to verify authorization
 client = speech_v1.SpeechClient()
+
 
 def sample_recognize(local_file_path):
     """
@@ -17,16 +24,13 @@ def sample_recognize(local_file_path):
     local_file_path : Path to local audio file (works for flac and wav files)
 
     :returns
-    Most probable transcription for given audio file.
+    Most probable transcription for given audio file
     """
-
     client = speech_v1.SpeechClient()
 
-    # The language of the supplied audio
-    language_code = "en-US"
+    language_code = "en-US"  # The language of the supplied audio
 
-    # Sample rate in Hertz of the audio data sent
-    sample_rate_hertz = 16000
+    sample_rate_hertz = 16000  # Sample rate in Hertz of the audio data sent
 
     # encoding = enums.RecognitionConfig.AudioEncoding.FLAC # add encoding type if not using flac or wav files
 
@@ -38,18 +42,19 @@ def sample_recognize(local_file_path):
 
     with io.open(local_file_path, "rb") as f:
         content = f.read()
-    audio = {"content": content}
 
-    response = client.recognize(config, audio)  # doing the transcription through Google Speech-to-text API
+    audio = {"content": content}
+    response = client.recognize(config, audio)  # doing the transcription through Google Speech-to-Text API
 
     # some API responses return no result/transcription
     try:
         for result in response.results:
             # First alternative is the most probable result (highest confidence)
             alternative = result.alternatives[0]
-        return alternative.transcript.upper() # converting to upper for uniformity in evaluation
+        return alternative.transcript.upper()  # converting to upper for uniformity in evaluation
     except:
         return " "  # returns empty string if the API returns no transcription
+
 
 path_txt = "./voxforge_sample_files/test/txt/"
 path_wav = "./voxforge_sample_files/test/wav/"
@@ -60,20 +65,24 @@ WER = []
 CER = []
 
 for each in tqdm(os.listdir(path_txt)):
+    # Read the original/gold-standard transcription
     with open(path_txt + each, "r") as file:
         text = file.read()
-        texts.append(text)
+    texts.append(text)
+    
+    # Function call to obtain decoded transcription from API
     tran = sample_recognize(path_wav + each[:-4] + ".wav")
     trans.append(tran)
 
+    # Calculate Character Error Rate (CER)
     cer = Levenshtein.distance(tran.replace(' ', ''), text.replace(' ', ''))
     CER = np.append(CER, cer)
+    
+    # Calculate Word Error Rate (WER)
     uniquewords = set(tran.split() + text.split())  # split output strings by spaces and create set of unique words
+    word2char = dict(zip(uniquewords, range(len(uniquewords))))  # create dictionary of unique words
 
-    word2char = dict(zip(uniquewords, range(len(uniquewords))))  # create dictionary of unique word
-
-    # map the words to a char array (Levenshtein packages only accepts strings)
-    # map words to index in the dictionary word2char
+    # map words to the word2char dictionary which is converted to character array (Levenshtein package only accepts strings)
     w1 = [chr(word2char[w]) for w in tran.split()]
     w2 = [chr(word2char[w]) for w in text.split()]
 
