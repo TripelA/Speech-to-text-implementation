914f807f760c876d4e6e585bba0007c0f2e3ac9a
admin <admin@SEAS13741.ead.gwu.edu>
2020-05-01 01:52:51 -0400

    added comments to train

--- code/train_stripped.py
+++ code/train_stripped.py
@@ -10,14 +10,14 @@ import torch.utils.data.distributed
 from apex import amp
 from apex.parallel import DistributedDataParallel
 from warpctc_pytorch import CTCLoss
-
-from data.data_loader import AudioDataLoader, SpectrogramDataset, BucketingSampler, DistributedBucketingSampler
-from decoder import GreedyDecoder
 from logger import VisdomLogger, TensorBoardLogger
+
+
+from data_loader_stripped import AudioDataLoader, SpectrogramDataset, BucketingSampler, DistributedBucketingSampler
+from decoder_stripped import GreedyDecoder
 from model import DeepSpeech, supported_rnns
 from test import evaluate
 from utils import reduce_tensor, check_loss, remove_parallel_wrapper
-
 
 torch.manual_seed(123456)
 torch.cuda.manual_seed_all(123456)
@@ -28,10 +28,13 @@ def to_np(x):
 
 
 class DefaultArguments():
+    """ Use this class to change values of model, data and training parameters"""
 
     def __init__(self):
         self.id = 'Deepspeech training'
-        self.train_manifest = 'data/voxforge_train_manifest.csv'
+
+        # TODO check paths
+        self.train_manifest = 'data/voxforge_train_manifest_v2.csv'
         self.val_manifest = 'data/voxforge_test_manifest.csv'
 
         # sampling_rate = 16k says that this audio was recorded(sampled) with a sampling frequency of 16k. In other
@@ -47,49 +50,51 @@ class DefaultArguments():
         self.hidden_size = 1000  # 'Hidden size of RNNs'
         self.hidden_layers = 5  # 'Number of RNN layers'
         self.rnn_type = 'lstm'  # 'Type of the RNN. rnn|gru|lstm are supported'
-        self.epochs = 50 # Number of training epochs
-        self.cuda = 'cuda' #Use cuda to train model'
-        self.lr = 3e-4 #'initial learning rate'
-        self.momentum = 0.9 #'momentum'
-        self.max_norm = 400 #'Norm cutoff to prevent explosion of gradients'
-        self.learning_anneal = 1.1 # 'Annealing applied to learning rate every epoch'
-        self.silent = False # 'Turn off progress tracking per iteration'
-        self.checkpoint = False # 'Enables checkpoint saving of model'
-        self.checkpoint_per_batch = 0 #Save checkpoint per batch. 0 means never save'
-        self.visdom = False # Turn on visdom graphing'
-        self.tensorboard = False #'Turn on tensorboard graphing'
-        self.log_dir = 'visualize/deepspeech_final' #'Location of tensorboard log'
-        self.log_params = False #'Log parameter values and gradients'
-        self.id = 'Deepspeech training' #'Identifier for visdom/tensorboard run'
-        self.save_folder = 'models/' # 'Location to save epoch models'
-        self.model_path = 'models/iteration2.pth' # 'Location to save best validation model'
-        self.continue_from = 'librispeech_pretrained_v2.pth' # continue from checkpoint model
-        self.finetune = True #'Finetune the model from checkpoint "continue_from"'
-        self.speed_volume_perturb = False # 'Use random tempo and gain perturbations.'
+        self.epochs = 30  # Number of training epochs
+        self.cuda = 'cuda'  # Use cuda to train model'
+        self.lr = 3e-4  # 'initial learning rate'
+        self.momentum = 0.9  # 'momentum'
+        self.max_norm = 400  # 'Norm cutoff to prevent explosion of gradients'
+        self.learning_anneal = 1.1  # 'Annealing applied to learning rate every epoch'
+        self.silent = False  # 'Turn off progress tracking per iteration'
+        self.checkpoint = False  # 'Enables checkpoint saving of model'
+        self.checkpoint_per_batch = 0  # Save checkpoint per batch. 0 means never save'
+        self.visdom = False  # Turn on visdom graphing'
+        self.tensorboard = False  # 'Turn on tensorboard graphing'
+        self.log_dir = 'visualize/deepspeech_final'  # 'Location of tensorboard log'
+        self.log_params = False  # 'Log parameter values and gradients'
+        self.id = 'Deepspeech training'  # 'Identifier for visdom/tensorboard run'
+        self.save_folder = 'models/'  # 'Location to save epoch models'
+        self.model_path = 'models/iteration5.pth'  # 'Location to save best validation model'
+        # TODO check path
+        self.continue_from = 'librispeech_pretrained_v2.pth'  # continue from checkpoint model
+        self.finetune = True  # 'Finetune the model from checkpoint "continue_from"'
+        self.speed_volume_perturb = False  # 'Use random tempo and gain perturbations.'
         self.spec_augment = 'Use simple spectral augmentation on mel spectograms.'
-        self.noise_dir = None # 'Directory to inject noise into audio. If default, noise Inject not added'
-        self.noise_prob = 0.4 #'Probability of noise being added per sample'
-        self.noise_min = 0.0 #'Minimum noise level to sample from. (1.0 means all noise, not original signal)'
-        self.noise_max = 0.5 # 'Maximum noise levels to sample from. Maximum 1.0'
-        self.no_shuffle = False # 'Turn off shuffling and sample from dataset based on sequence length (smallest to largest)'
-        self.no_sortaGrad = False # 'Turn off ordering of dataset on sequence length for the first epoch.'
-        self.bidirectional = True #'Turn off bi-directional RNNs, introduces lookahead convolution'
-        self.dist_url = 'tcp://127.0.0.1:1550' # 'url used to set up distributed training'
-        self.dist_backend = 'nccl' # distributed backend
-        self.world_size = 1 # 'number of distributed processes'
-        self.rank = 0 # 'The rank of this process'
-        self.gpu_rank = None # 'If using distributed parallel for multi-gpu, sets the GPU for the process'
-        self.seed = 123456  #Seed to generators
+        self.noise_dir = None  # 'Directory to inject noise into audio. If default, noise Inject not added'
+        self.noise_prob = 0.4  # 'Probability of noise being added per sample'
+        self.noise_min = 0.0  # 'Minimum noise level to sample from. (1.0 means all noise, not original signal)'
+        self.noise_max = 0.5  # 'Maximum noise levels to sample from. Maximum 1.0'
+        self.no_shuffle = False  # 'Turn off shuffling and sample from dataset based on sequence length (smallest to
+        # largest)'
+        self.no_sortaGrad = False  # 'Turn off ordering of dataset on sequence length for the first epoch.'
+        self.bidirectional = True  # 'Turn off bi-directional RNNs, introduces lookahead convolution'
+        self.dist_url = 'tcp://127.0.0.1:1550'  # 'url used to set up distributed training'
+        self.dist_backend = 'nccl'  # distributed backend
+        self.world_size = 1  # 'number of distributed processes'
+        self.rank = 0  # 'The rank of this process'
+        self.gpu_rank = None  # 'If using distributed parallel for multi-gpu, sets the GPU for the process'
+        self.seed = 123456  # Seed to generators
         self.opt_level = 'O1'
         self.keep_batchnorm_fp32 = None
-        self.loss_scale = 1 # Loss scaling used by Apex. Default is 1 due to warp-ctc not supporting scaling of gradients'
+        self.loss_scale = 1  # Loss scaling used by Apex. Default is 1 due to warp-ctc not supporting scaling of
+        # gradients'
         self.distributed = False
         self.no_sorta_grad = False
 
 
-
 class AverageMeter(object):
-    """Computes and stores the average and current value"""
+    """Computes and stores the average and current value, used for evaluation and epoch time computation and ctc loss"""
 
     def __init__(self):
         self.reset()
@@ -108,6 +113,8 @@ class AverageMeter(object):
 
 
 if __name__ == '__main__':
+
+    # load the default arguments
     args = DefaultArguments()
 
     # Set seeds for determinism
@@ -146,7 +153,6 @@ if __name__ == '__main__':
     # if main_proc and args.tensorboard:
     #     tensorboard_logger = TensorBoardLogger(args.id, args.log_dir, args.log_params)
 
-
     avg_loss, start_epoch, start_iter, optim_state, amp_state = 0, 0, 0, None, None
 
     # start from the pretrained models
@@ -162,7 +168,6 @@ if __name__ == '__main__':
         # set labels A-Z, -, ' ', total 29
         labels = model.labels
 
-
         audio_conf = model.audio_conf
 
         if not args.finetune:  # Don't want to restart training
@@ -175,7 +180,11 @@ if __name__ == '__main__':
                 start_iter = 0
             else:
                 start_iter += 1
+
+            # get what was the last avg loss
             avg_loss = int(package.get('avg_loss', 0))
+
+            # get evaluation metrics for ctc loss, wer, cer
             loss_results, cer_results, wer_results = package['loss_results'], package['cer_results'], \
                                                      package['wer_results']
             best_wer = wer_results[start_epoch]
@@ -185,9 +194,11 @@ if __name__ == '__main__':
             #     tensorboard_logger.load_previous_values(start_epoch, package)
     # train new model
     else:
+        # read labels
         with open(args.labels_path) as label_file:
             labels = str(''.join(json.load(label_file)))
 
+        # create audio configuration dictionary
         audio_conf = dict(sample_rate=args.sample_rate,
                           window_size=args.window_size,
                           window_stride=args.window_stride,
@@ -196,8 +207,10 @@ if __name__ == '__main__':
                           noise_prob=args.noise_prob,
                           noise_levels=(args.noise_min, args.noise_max))
 
+        # rnn type either GRU or LSTM
         rnn_type = args.rnn_type.lower()
         assert rnn_type in supported_rnns, "rnn_type should be either lstm, rnn or gru"
+
         # create network architecture
         model = DeepSpeech(rnn_hidden_size=args.hidden_size,
                            nb_layers=args.hidden_layers,
@@ -223,6 +236,7 @@ if __name__ == '__main__':
     if not args.distributed:
         train_sampler = BucketingSampler(train_dataset, batch_size=args.batch_size)
     else:
+        # if we are using distributed programing on multiple GPUs
         train_sampler = DistributedBucketingSampler(train_dataset, batch_size=args.batch_size,
                                                     num_replicas=args.world_size, rank=args.rank)
 
@@ -231,14 +245,13 @@ if __name__ == '__main__':
                                    num_workers=args.num_workers, batch_sampler=train_sampler)
     test_loader = AudioDataLoader(test_dataset, batch_size=args.batch_size,
                                   num_workers=args.num_workers)
-
+    # shuffle the batches after every epoch to improve the performance
     if (not args.no_shuffle and start_epoch != 0) or args.no_sorta_grad:
         print("Shuffling batches for the following epochs")
         train_sampler.shuffle(start_epoch)
-    # print(device)
+
     model = model.to(device)
     parameters = model.parameters()
-
 
     # Declare model and optimizer as usual, with default (FP32) precision
     optimizer = torch.optim.SGD(parameters, lr=args.lr,
@@ -252,9 +265,11 @@ if __name__ == '__main__':
                                       keep_batchnorm_fp32=args.keep_batchnorm_fp32,
                                       loss_scale=args.loss_scale)
 
+    # load optimizer state
     if optim_state is not None:
         optimizer.load_state_dict(optim_state)
 
+    # load precision state
     if amp_state is not None:
         amp.load_state_dict(amp_state)
 
@@ -263,15 +278,20 @@ if __name__ == '__main__':
     print(model)
     print("Number of parameters: %d" % DeepSpeech.get_param_size(model))
 
+    # create class objects
     criterion = CTCLoss()
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
 
+    # start the training epoch
     for epoch in range(start_epoch, args.epochs):
+
         model.train()
         end = time.time()
         start_epoch_time = time.time()
+
+        # load data using generator audio data loader
         for i, (data) in enumerate(train_loader, start=start_iter):
             if i == len(train_sampler):
                 break
@@ -287,21 +307,24 @@ if __name__ == '__main__':
             data_time.update(time.time() - end)
             inputs = inputs.to(device)
 
-
+            # model outputs batch * max seq length (T) * 29(labels)
             out, output_sizes = model(inputs, input_sizes)
             out = out.transpose(0, 1)  # TxNxH
 
             float_out = out.float()  # ensure float32 for loss
+
+            # calculate ctc loss
             loss = criterion(float_out, targets, output_sizes, target_sizes).to(device)
             loss = loss / inputs.size(0)  # average the loss by minibatch
 
+            # if distributed gather ctc loss
             if args.distributed:
                 loss = loss.to(device)
                 loss_value = reduce_tensor(loss, args.world_size).item()
             else:
                 loss_value = loss.item()
 
-            # Check to ensure valid loss was calculated
+            # Check to ensure valid loss was calculated, there is no inf or nan
             valid_loss, error = check_loss(loss, loss_value)
             if valid_loss:
                 optimizer.zero_grad()
@@ -316,18 +339,22 @@ if __name__ == '__main__':
                 print('Skipping grad update')
                 loss_value = 0
 
+            # add epoch loss
             avg_loss += loss_value
             losses.update(loss_value, inputs.size(0))
 
             # measure elapsed time
             batch_time.update(time.time() - end)
             end = time.time()
+
+            # print the output on the console
             if not args.silent:
                 print('Epoch: [{0}][{1}/{2}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
                     (epoch + 1), (i + 1), len(train_sampler), batch_time=batch_time, data_time=data_time, loss=losses))
+            # if you want to save output after every batch, default set to 0
             if args.checkpoint_per_batch > 0 and i > 0 and (i + 1) % args.checkpoint_per_batch == 0 and main_proc:
                 file_path = '%s/deepspeech_checkpoint_epoch_%d_iter_%d.pth' % (save_folder, epoch + 1, i + 1)
                 print("Saving checkpoint model to %s" % file_path)
@@ -343,6 +370,7 @@ if __name__ == '__main__':
                            file_path)
             del loss, out, float_out
 
+        # average loss across all batches
         avg_loss /= len(train_sampler)
 
         epoch_time = time.time() - start_epoch_time
@@ -351,6 +379,7 @@ if __name__ == '__main__':
               'Average Loss {loss:.3f}\t'.format(epoch + 1, epoch_time=epoch_time, loss=avg_loss))
 
         start_iter = 0  # Reset start iteration for next epoch
+        # evalulate results on test dataset
         with torch.no_grad():
             wer, cer, output_data = evaluate(test_loader=test_loader,
                                              device=device,
@@ -380,6 +409,7 @@ if __name__ == '__main__':
         #         'Avg CER': cer
         #     }
 
+        # if you have to save file after every epoch
         if main_proc and args.checkpoint:
             file_path = '%s/deepspeech_%d.pth.tar' % (save_folder, epoch + 1)
             torch.save(DeepSpeech.serialize(remove_parallel_wrapper(model),
@@ -390,11 +420,13 @@ if __name__ == '__main__':
                                             wer_results=wer_results,
                                             cer_results=cer_results),
                        file_path)
-        # anneal lr
+        # anneal lr Learning rate annealing is reducing the rate after every epoch in order to not miss the local
+        # minimum and avoid oscillation
         for g in optimizer.param_groups:
             g['lr'] = g['lr'] / args.learning_anneal
         print('Learning rate annealed to: {lr:.6f}'.format(lr=g['lr']))
 
+        # if the best model is found than previous iteration, overwrite the model with better model
         if main_proc and (best_wer is None or best_wer > wer):
             print("Found better validated model, saving to %s" % args.model_path)
             torch.save(DeepSpeech.serialize(remove_parallel_wrapper(model),
@@ -407,6 +439,7 @@ if __name__ == '__main__':
             best_wer = wer
             avg_loss = 0
 
+        # if you want to shuffle argument after every epoch
         if not args.no_shuffle:
             print("Shuffling batches...")
             train_sampler.shuffle(epoch)
